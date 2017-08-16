5650f8ed88b0442fcfbd9327643e34158e5cc9a9
zhangli <zhangli@kuaiqiangche.com>
2017-08-16 09:42:42 +0800

    update

--- src/components/countdown.vue
+++ src/components/countdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index">
-       <canvas id="countDown" height="800" width="1024"  style="display:block;margin:0 auto;border:1px solid #ccc;" ></canvas> 
+       <canvas id="countDown" height="400" width="1024"  style="display:block;margin:0 auto;border:1px solid #ccc;" ></canvas> 
   </div>
 </template>
 
@@ -19,17 +19,19 @@ export default {
       RADIUS:6,
       MARGIN_TOP:60,
       MARGIN_LEFT:30,
-      endTime: new Date(2017,7,15,17,30,55),
-      ret:0,
+      endTime: new Date(2017,7,16,17,30,55),
       curShowTimeSeconds:0,
-      nextShowTimeSeconds:0
+      nextShowTimeSeconds:0,
+      colors:["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"],
+      balls:[],
     }
   },
   mounted:function (){
     var dom = document.getElementById('countDown');
     this.ctx = dom.getContext("2d");
     this.renderData(this.ctx);
-    var that = this
+    var that = this;
+    this.curShowTimeSeconds = this.getCurrentShowTimeSeconds();
     setInterval(function(){
       that.renderData(that.ctx);
       that.update();
@@ -37,10 +39,8 @@ export default {
   },
   methods:{
     renderData(ctx){
-
       ctx.clearRect(0,0,1024,1024);
-      this.getCurrentShowTimeSeconds();
-      this.curShowTimeSeconds = this.ret;
+      
       var hours = parseInt(this.curShowTimeSeconds/3600);
       var minutes = parseInt((this.curShowTimeSeconds - hours*3600)/60);
       var seconds = this.curShowTimeSeconds%60;
@@ -53,6 +53,14 @@ export default {
       this.renderDigit (this.MARGIN_LEFT+69*(this.RADIUS+1),this.MARGIN_TOP,10,ctx);
       this.renderDigit (this.MARGIN_LEFT+78*(this.RADIUS+1),this.MARGIN_TOP,parseInt(seconds/10),ctx);
       this.renderDigit (this.MARGIN_LEFT+93*(this.RADIUS+1),this.MARGIN_TOP,parseInt(seconds%10),ctx);
+
+      for (var i = 0; i < this.balls.length; i++) {
+        ctx.fillStyle = this.balls[i].color;
+        ctx.beginPath();
+        ctx.arc(this.balls[i].x,this.balls[i].y,this.RADIUS,0,2*Math.PI,false);
+        ctx.closePath();
+        ctx.fill();
+      }
     },
     renderDigit(x,y,num,ctx){
       //绘制数字
@@ -70,13 +78,13 @@ export default {
     },
     getCurrentShowTimeSeconds(){
       var curTime = new Date();//获取当前时间
-      this.ret = this.endTime.getTime() - curTime.getTime();//获取倒计时毫秒数
-      this.ret = Math.round(this.ret/1000)//转换成秒数
-      this.ret > 0 ? this.ret : 0;  
+      var ret = this.endTime.getTime() - curTime.getTime();//获取倒计时毫秒数
+      ret = Math.round(ret/1000)//转换成秒数
+      ret > 0 ? ret : 0;  
+      return ret;
     },
     update(){
-      this.getCurrentShowTimeSeconds();
-      this.nextShowTimeSeconds = this.ret;
+      this.nextShowTimeSeconds = this.getCurrentShowTimeSeconds();
       var nextHours = parseInt(this.nextShowTimeSeconds/3600);
       var nextMinutes = parseInt((this.nextShowTimeSeconds - nextHours*3600)/60);
       var nextSeconds = this.nextShowTimeSeconds%60;
@@ -89,8 +97,71 @@ export default {
         this.curShowTimeSeconds = this.nextShowTimeSeconds;
       }
 
+      if(parseInt(curHours/10) != parseInt(nextHours/10) ){
+        this.addBalls(this.MARGIN_LEFT,this.MARGIN_TOP,parseInt(curHours/10));
+        console.log(1)
+      }
+      if(parseInt(curHours%10) != parseInt(nextHours%10)){
+        this.addBalls(this.MARGIN_LEFT+15*(this.RADIUS+1),this.MARGIN_TOP,parseInt(curHours%10));
+        console.log(2)
+      }
+      if(parseInt(curMinutes/10) != parseInt(nextMinutes/10)){
+        this.addBalls(this.MARGIN_LEFT+39*(this.RADIUS+1),this.MARGIN_TOP,parseInt(curMinutes/10));
+        console.log(3)
+      }
+      if(parseInt(curMinutes%10) != parseInt(nextMinutes%10)){
+        this.addBalls(this.MARGIN_LEFT+54*(this.RADIUS+1),this.MARGIN_TOP,parseInt(curMinutes%10));
+        console.log(4)
+      }
+      if(parseInt(curSeconds/10) != parseInt(nextSeconds/10)){
+        this.addBalls(this.MARGIN_LEFT+78*(this.RADIUS+1),this.MARGIN_TOP,parseInt(curSeconds/10) );
+        console.log(5)
+      }
+      if(parseInt(curSeconds%10) != parseInt(nextSeconds%10)){
+        this.addBalls(this.MARGIN_LEFT+93*(this.RADIUS+1),this.MARGIN_TOP,parseInt(curSeconds%10) );
+        console.log(6)
+      }
 
+      this.updateBalls();
+    },
+    addBalls(x,y,num){
+      for (var i = 0; i < this.digit[num].length; i++) {
+        for (var j = 0; j < this.digit[num][i].length; j++) {
+          if(this.digit[num][i][j] ===1){
+            var aball = {
+              x: x + 2*j*(this.RADIUS+1)+(this.RADIUS+1),
+              y: y + 2*i*(this.RADIUS+1)+(this.RADIUS+1),
+              g:1.5+Math.random(),
+              vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
+              vy:-5,
+              color:this.colors[Math.floor(Math.random()*this.colors.length)]
+            }
+            this.balls.push(aball);
+          }
+        }
+      }
+    },
+    updateBalls(){
+     for (var i = 0; i < this.balls.length; i++) {
+       this.balls[i].x += this.balls[i].vx;
+       this.balls[i].y += this.balls[i].vy;
+       this.balls[i].vy += this.balls[i].g;
+
+       if(this.balls[i].y >= (800-this.RADIUS)){
+        this.balls[i].y = 800-this.RADIUS;
+        this.balls[i].vy = -this.balls[i].vy*0.75;
+       }
+     }
+
+    var cnt = 0
+    for( var i = 0 ; i < this.balls.length ; i ++ )
+        if( this.balls[i].x + this.RADIUS > 0 && this.balls[i].x -this.RADIUS < 1024 )
+            this.balls[cnt++] = this.balls[i]
+
+    while( this.balls.length > cnt ){
+        this.balls.pop();
     }
+    },
   }
 }
 </script>
